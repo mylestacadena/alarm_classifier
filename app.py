b816e2d6792fc88965eb52ec2afde49296e9cac9
mylestacadena <mylestacadena904@gmail.com>
2025-06-12 23:55:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ with st.sidebar:
     selected_page = option_menu(
         menu_title="Alarm Sound Classifier",
         options=[
-            "Home",
+            "Dashboard",
             "Upload Audio File",
             "Use Microphone",
         ],
@@ -49,7 +49,7 @@ with st.sidebar:
         styles={
             "container": {
                 "padding": "20px",
-                "background-color": "#6C63FF",
+                "background-color": "#164b82",
                 "border-radius": "20px",
             },
             "icon": {"color": "white", "font-size": "18px"},
@@ -152,7 +152,7 @@ def extract_features(file_path):
     return np.array(list(features.values())).reshape(1, -1)
 
 # === Page Logic ===
-if selected_page == "Home":
+if selected_page == "Dashboard":
     st.markdown('<div class="app-title">Alarm Sound Classifier</div>', unsafe_allow_html=True)
     st.markdown("Welcome to the alarm sound classifier. Choose a mode on the left.")
 
